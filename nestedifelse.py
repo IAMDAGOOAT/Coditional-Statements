4ef73c72ddba7e5d24b01dde33bc561287c676f8
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-19 12:51:07 +0800

    netsted if else

--- nestedifelse.py
+++ nestedifelse.py
@@ -6,3 +6,13 @@ if (i==13):
        print("i is smaller than 13 too")
     else:
        print("i is greater than 12 and smaller than 13")
+       
+n = int(input("Enter a number: "))
+
+if n >= 0:
+    if n == 0:
+        print("Number is zero")
+    else:
+        print("Number is positive")
+else:
+    print("Number is negative")
